071e96092b4dff617e9279a6e14d18d6e9bacbee
VeraBu <opilki-in-yhe-head@mail.ru>
2023-04-14 23:54:55 +0300

    corr main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import pack1.Cat;
+import pack1.Fox;
 
 public class Main {
     /*
@@ -15,6 +16,8 @@ public class Main {
 
         Notebook note1 = new Notebook(8, 512, "Windows 11", "white");
         Cat ca1 = new Cat("Пушок", "Игорь", "белый");
+        Fox fox1 = new Fox("Nick", 7, "orange");
+        System.out.println(fox1);
         System.out.println(ca1);
         System.out.println(note1);
 
